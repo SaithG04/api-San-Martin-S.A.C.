c2a8f27b6e2ce1240a7b88017b790684957641e8
Isaí Quiroga <isai.quiroga.2004@gmail.com>
2024-11-07 21:50:18 -0500

    feat(User Management): Implement user registration feature
    - Added User model with encrypted password and role management
    - Created UserRepository for CRUD operations
    - Developed UserService to handle registration logic, including username uniqueness validation and password encryption with BCrypt
    - Exposed /api/users/register endpoint in UserController for user registration
    - Configured SecurityConfig to allow public access to the /register endpoint

---
+++ user-management/src/main/java/com/risk/user_management/domain/User.java
@@ -0,0 +1,24 @@
+package com.risk.user_management.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
---
+++ user-management/src/main/java/com/risk/user_management/domain/UserRole.java
@@ -0,0 +1,7 @@
+package com.risk.user_management.domain;
+
+public enum UserRole {
+    ADMIN,
+    USER,
+    AUDITOR
+}
